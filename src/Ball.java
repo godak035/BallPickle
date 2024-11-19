936e217047fb61152518db7ba66a776710042f58
Avishan <avishan035@gmail.com>
2024-11-19 13:01:32 -0500

    Co-authored-by: David SUE <339759227@GoTVDSB.ca> Co-authored-by: Owen MCCARTHY <339772246@GoTVDSB.ca>

---
+++ src/Ball.java
@@ -0,0 +1,13 @@
+public class Ball {
+    int x, y; //pixel position of ball
+    double xx, yy; //precise position of ball
+    double vx, yx; //velocity of ball
+    
+    public void move(){
+        
+    }
+
+    public void reset(){
+        
+    }
+}
