44f146c0264727d7f34cc5d698aca49ee56377a2
Avishan <avishan035@gmail.com>
2024-11-29 13:04:37 -0500

    I like to score

---
+++ src/Score.java
@@ -0,0 +1,22 @@
+public class Score {
+    
+    int playerScore, enemyScore;
+    int maxScore = 5;    
+    
+    
+    /**
+     * 
+     */
+    
+    //IF PLAYER SCORE then PLAYER.Score++:
+  
+    //IF Enemy SCore the Enemey.Score++:
+    
+    //IF Player or Enemy score == 5 then Game ends
+    //Check IF player reached 5 then Player wins. 
+    //check if Enemy reachedd 5 then Player loses. Mock the player for s
+    //Check Win conditions. 
+        
+    //
+
+}
