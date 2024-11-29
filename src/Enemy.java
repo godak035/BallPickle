b5bf51eca9cb56d841ddadf66a23fc46986e0018
Avishan <avishan035@gmail.com>
2024-11-29 12:56:06 -0500

    Co-authored-by: Vadim MIRONOV <362941619@GoTVDSB.ca> Co-authored-by: Owen-McCarthy <omccarthy2008@gmail.com>

---
+++ src/Enemy.java
@@ -0,0 +1,28 @@
+public class Enemy extends Entity {
+     int size;
+     int level;
+
+
+    /**
+    * enemies on our six!!!!!!!!!!!!!!!!!!
+    * @param xx
+    * @param yy
+    * @param v
+    * @param s
+    */
+    public Enemy(double xx, double yy, double v, int s, int level) {
+        super(xx, yy, v);
+        this.size = s;
+    }
+
+
+    @Override
+    void updatePosition() {
+        this.x = (int)xx;
+        this.y = (int)yy;
+    }
+
+
+
+
+}
